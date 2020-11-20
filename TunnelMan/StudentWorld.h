ae5e5d8c8d4973cefd4161ad3245e9f7d562e36b
Sooihk Ro <aduroro@gmail.com>
2020-11-20 01:05:32 -0800

    update test

--- TunnelMan/StudentWorld.h
+++ TunnelMan/StudentWorld.h
@@ -44,6 +44,7 @@ public:
 		return actors;
 	}
 	// new changesaaaa
+	// yep new one
 private:
 	std::vector<Actor*> actors; // vector containing pointers to actor classes
 	TunnelMan* tunnelplayer; // pointer to TunnelMan class
